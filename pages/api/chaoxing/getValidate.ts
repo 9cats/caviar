626c4752377f1253783334cdd1e4def7353364b9
ninecats <123337671@qq.com>
2022-08-31 18:08:57 +0800

    :sparkles: 添加增加验证授权缓存接口

---
+++ pages/api/chaoxing/getValidate.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { ResponseType } from "../_type";
+import * as superagent from "superagent";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  let {
+    num = "", // 用户名
+  } = req.query;
+
+  if (!num) {
+    return res.status(200).json({
+      success: false,
+      data: "未填写参数",
+    });
+  }
+
+  let agent = superagent.agent(); //agent
+
+  let result = await agent
+    .get(`http://chaoxing_slidecaptcha_verify:8888/validate/get?num=${num}`)
+    .then(async (res) => {
+      console.log(res.body);
+      if (res.body.success) {
+        return {
+          success: true,
+          data: "null",
+        };
+      } else {
+        return {
+          success: false,
+          data: "null",
+        };
+      }
+    })
+    .catch((e) => {
+      return {
+        success: false,
+        data: "error",
+      };
+    });
+
+  return res.status(200).json(result);
+}
